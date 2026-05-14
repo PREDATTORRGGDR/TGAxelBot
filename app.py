234f12a6d80165274f55a8b9c5695cedc8f74fdd
Kyrapyto <nikitawork466@gmail.com>
2026-05-15 00:34:12 +0300

    create base bot with answer

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 import telebot
-
+from telebot.types import Message
 bot = telebot.TeleBot('8910131268:AAHxGzYIkzkuO_KAE5Po2M-rEBz6hkpez5I')
+
+@bot.message_handler(commands=['start'])
+def cmd_start(message: Message):
+    user_name = message.from_user.username
+    text = (
+        f"Привет {user_name}! \n"
+        f"Я - бот и меня зовут Майк Вазовский."
+    )
+    bot.send_message(message.chat.id, text)
+
+bot.polling()
+
