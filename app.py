5b2237dd1d63e130b42fc522c99a987d0c3756a0
Kyrapyto <nikitawork466@gmail.com>
2026-05-17 13:15:14 +0300

    create mini-game; coin

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import telebot
+import telebot, random
 from telebot.types import Message
 bot = telebot.TeleBot('8910131268:AAHxGzYIkzkuO_KAE5Po2M-rEBz6hkpez5I')
 
@@ -7,13 +7,22 @@ def cmd_start(message: Message):
     user_name = message.from_user.username
     text = (
         f"Привет {user_name}! \n"
-        f"Я - бот и меня зовут Майк Вазовский. \n"
-        f"\n"
+        f"Я - бот и меня зовут Майк Вазовский👋\n"
+        f"/coin - бросить монету\n"
         f"\n"
         f"/help - показать cписок команд"
 
     )
     bot.send_message(message.chat.id, text)
 
-bot.polling()
+
+
+@bot.message_handler(commands=['coin'])
+def cmd_coin(message: Message): 
+    result = random.choice(["Орел🦅", "Решка🪙"])
+    bot.reply_to(message, f"Выпало: {result}")
+
+if __name__ == '__main__':
+    # start the bot
+    bot.polling(none_stop=True)
 
