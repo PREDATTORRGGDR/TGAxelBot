f6479eff243378d760e0d5be89ce089bd687fd50
Kyrapyto <nikitawork466@gmail.com>
2026-05-17 13:32:56 +0300

    fix bags

--- app.py
+++ app.py
@@ -40,23 +40,16 @@ def cmd_dice(message: Message):
 @bot.message_handler(commands=['clean'])
 def cmd_clean(message: Message):
     chat_id = message.chat.id
-    # Try to delete recent messages in the chat. Bots can only delete messages they have permission to delete
-    # (in groups/supergroups they must be admin with delete permission; in private chats they can delete their own messages).
-    # This attempts to delete up to 100 previous messages.
+
     start_id = message.message_id
     end_id = max(start_id - 100, 0)
     for msg_id in range(start_id, end_id, -1):
         try:
             bot.delete_message(chat_id, msg_id)
         except Exception:
-            # ignore messages the bot can't delete
+
             pass
-    # send confirmation (will be a new message)
-    bot.send_message(chat_id, "Чат очищен!")
-
-
-
-
+   
 
 
 
